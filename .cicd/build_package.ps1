47ed0d0d2f02362306cffc1b5f67f3521df2038c
Eric Bezault <ericb@gobosoft.com>
2026-01-11 18:35:56 +0100

    Added a release pipeline to create a release when a tag of the form `vX.Y.Z` is created.

--- .cicd/build_package.ps1
+++ .cicd/build_package.ps1
@@ -6,24 +6,34 @@
 	Build Gobo Eiffel VS Code extension package.
 
 .EXAMPLE
-	# Build Gobo Eiffel VS Code extension package:
+	# Build Gobo Eiffel VS Code extension nightly build package:
 	build_package.ps1
+
+.EXAMPLE
+	# Build Gobo Eiffel VS Code extension release package:
+	build_package.ps1 1.2.3
 
 .NOTES
 	Copyright: "Copyright (c) 2026, Eric Bezault and others"
 	License: "MIT License"
 #>
 
+param
+(
+	[Parameter(Mandatory=$false)]
+	[string] $Version
+)
+
 $ErrorActionPreference = "Stop"
 
-& "$PSScriptRoot/set_package_version.ps1" package.json
+& "$PSScriptRoot/set_package_version.ps1" package.json $Version
 if ($LastExitCode -ne 0) {
-	Write-Error "Command 'set_package_version.ps1 package.json' exited with code $LastExitCode"
+	Write-Error "Command 'set_package_version.ps1 package.json $Version' exited with code $LastExitCode"
 	exit $LastExitCode
 }
-& "$PSScriptRoot/set_package_version.ps1" package-lock.json
+& "$PSScriptRoot/set_package_version.ps1" package-lock.json $Version
 if ($LastExitCode -ne 0) {
-	Write-Error "Command 'set_package_version.ps1 package-lock.json' exited with code $LastExitCode"
+	Write-Error "Command 'set_package_version.ps1 package-lock.json $Version' exited with code $LastExitCode"
 	exit $LastExitCode
 }
 
